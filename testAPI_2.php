0a8ef009f015b15eceeb2c891a2c64b36de24b75
JeongwonJeong <je970311@gmail.com>
2022-12-13 12:43:37 +0900

    작업중 오류 발생 해당 작업 파일 실행시 기존의 실행되던 파일도 실행되지 않는 문제 발생 해결중

---
+++ testAPI_2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>API활용 지도 출력</title>
+    <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=be3a1d6c5b6bf597e6aea03d01ceb7b5"></script>
+  </head>
+  <body>
+    <div id="map" style="width:100%;height:400px;"></div>
+    <script>
+      var testmap = document.getElementById('map'),
+      mapoption = {
+        center: new kakao.maps.LatLng(33.450701, 126.570667),
+        level: 3
+      }
+
+      var map = new kakao.maps.Map(testmap, mapoption)
+    </script>
+
+    <?php
+    //? 해당 작업 뭔가 문제가 존재 그 전에 돌아가던 PHP 파일도 현 파일 실행시 실행되지 않는 오류 확인
+    ?>
+  </body>
+</html>
