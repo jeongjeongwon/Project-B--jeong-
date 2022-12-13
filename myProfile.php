09869410643a0a87ade6a8fb3157007f8fc76834
unknown <je970311@gmail.com>
2022-12-14 00:58:12 +0900

    주석처리

--- myProfile.php
+++ myProfile.php
@@ -26,15 +26,16 @@
         echo "제 취미는 {$this->hobby}이며, 제가 만들고 싶은 것은 {$this->want}입니다.";
       }
     }
-    class myName extends jeongwon{
+    class whoAreYou extends jeongwon{
       public function message(){
         echo "<h1>저는 누구인가요?</h1><br>";
       }
     }
-    $ThisWarofMine = new myName("정정원", "26살", "남자", "소설읽기와 다큐멘터리 시청", "국내뿐만 아니라 해외에서도 사람들이 이용하는 대규모 커뮤니티 사이트");
-    $ThisWarofMine->message();
+    $myName = new whoAreYou("정정원", "26살", "남자", "소설읽기와 다큐멘터리 시청", "국내뿐만 아니라 해외에서도 사람들이 이용하는 대규모 커뮤니티 사이트");
+    $myName->message();
 
     //? 클레스에서 변수와 상수를 정의하고 함수또한 정의하기도 한다. 이떄 instance는 이 클레스를 가져다 사용하는 것이다.
+    //* jeongwon이라는 클레스가 선언이 되어 있고 이를 whoAreYou 라는 클래스 객체가 상속받아 사용한다 이때 myName은 whoAreYou의 인스턴스이다.
     ?>
   </body>
 </html>
