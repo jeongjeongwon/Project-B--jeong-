4ae84b642674540035f1695c978bc53905b89daf
JeongwonJeong <je970311@gmail.com>
2022-12-14 18:17:14 +0900

    시작점과 도착점 마커 설정

--- testDraggable.php
+++ testDraggable.php
@@ -6,6 +6,7 @@
   </head>
   <body>
     <div id="map" style="width:500px;height:500px;"></div>
+    <p id="result"></p>
     <script>
       let mapData = document.getElementById('map');
       mapOption={
@@ -13,6 +14,33 @@
         level: 3
       }
       let map = new kakao.maps.Map(mapData, mapOption)
+
+      kakao.maps.event.addListener(map, 'click', function(mouseEvent){
+        let latlng = mouseEvent.latLng;
+
+        let message = "위도" + latlng.getLat()
+        message += "경도" + latlng.getLng()
+
+        let result = document.getElementById('result');
+        result.innerHTML = message
+      })
+
+      let startPosition = new kakao.maps.LatLng(36.349264, 127.377699);
+
+      let startMarker = new kakao.maps.Marker({
+        map: map,
+        position: startPosition,
+        draggable: true
+      })
+
+      let endPosition = new kakao.maps.LatLng(36.349538, 127.437166);
+
+      let endMarker = new kakao.maps.marker({
+        map: map,
+        position: endPosition,
+        draggable: true
+      })
+
     </script>
   </body>
 </html>
